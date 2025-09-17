fc67d03aa5d756bfcc1fdb6d06df6fc9acfde4fc
Naman-ap <141435865+Naman-ap@users.noreply.github.com>
2025-09-18 03:05:07 +0530

    New UI

--- src/app/commoditiy/commoditiy.component.ts
+++ src/app/commoditiy/commoditiy.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Commodity {
+  product: string;
+  expiry: string;
+  change: number;
+  per_change: number;
+}
 
 @Component({
   selector: 'app-commoditiy',
   templateUrl: './commoditiy.component.html',
   styleUrls: ['./commoditiy.component.css']
 })
-export class CommoditiyComponent {
+export class CommoditiyComponent implements AfterViewInit {
+  payload: any;
+  loading: boolean = true;
 
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit(): void {
+    this.http.get<any>('http://localhost:8000/commodities')
+      .subscribe({
+        next: (res) => {
+          this.payload = res.commodities;
+          this.loading = false;
+          console.log(this.payload)
+        },
+        error: (err) => {
+          console.error('Error fetching commodities data', err);
+          this.payload =  [];
+          this.loading = false;
+        }
+      });
+  }
 }
--- src/app/ipo/ipo.component.ts
+++ src/app/ipo/ipo.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Signup {
+  email: string;
+  password: string;
+}
+
+interface DashboardPayload {
+  top_gainers: any[];
+  top_losers: any[];
+  news: any[];
+}
 
 @Component({
   selector: 'app-ipo',
   templateUrl: './ipo.component.html',
   styleUrls: ['./ipo.component.css']
 })
-export class IpoComponent {
+export class IpoComponent implements AfterViewInit {
+  showSignup = false;
+  signup: Signup = { email: '', password: '' };
 
+  payload: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit(): void {
+
+    this.http.get<any>('http://localhost:8000/ipo')
+    .subscribe({
+      next: (res) => {
+        this.payload = res.ipo;
+        console.log("Data",this.payload)
+      },
+      error: (err) => {
+        console.error('Error fetching dashboard data', err);
+      }
+    });
+
+  }
+
+  toggleSignup(): void {
+    this.showSignup = !this.showSignup;
+  }
+
+  onSignup(): void {
+    alert(`Signed up with email: ${this.signup.email}`);
+  }
+
+  private loadDashboard(): void {
+    this.http.get<any>('http://localhost:8000/ipo')
+      .subscribe({
+        next: (res) => {
+          this.payload = res;
+          
+        },
+        error: (err) => {
+          console.error('Error fetching dashboard data', err);
+        }
+      });
+  }
 }
